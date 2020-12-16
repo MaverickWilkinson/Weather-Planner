a07d825580a9f36a28c4c1fcb19309e2b01a69e5
KingMaverick <maverickwilkinson@gmail.com>
2020-12-15 17:29:08 -0800

    successfully integrated api usage for gathering data and updated visuals. decided to stick to just css and html for consistency for now unless I really want to add something I cant do without it.

--- Develop/script.js
+++ Develop/script.js
@@ -1,17 +1,50 @@
-$.ajax({
-    url: "https://api.openweathermap.org/data/2.5/onecall?lat=47.60&lon=-122.33&appid=b45ecc5406887e49a29cd75e6b8dc427",
-    method: "GET"
-  }).then(function(response) {
-    console.log(response.Object.humidity);
-  });
-
 const cities = ['Austin','Chicago',"New York","Orlando","San Francisco","Seattle","Denver","Atlanta"];
 
 $(document).ready(function(){
 displayButtons();
+getWeatherData("Redmond");
 })
 
+// icon link
+// http://openweathermap.org/img/wn/04d@2x.png
 
+function getWeatherData(cityname){
+    // api.openweathermap.org/data/2.5/weather?q={city name}&appid={API key}
+
+    $.ajax({
+        url: `https://api.openweathermap.org/data/2.5/weather?q=${cityname}&units=imperial&appid=b45ecc5406887e49a29cd75e6b8dc427`,
+        method: "GET"
+      }).then(function(response) {
+          console.log(response)
+          let temp = response.main.temp;
+          let windspeed = response.wind.speed;
+          let humidity = response.main.humidity;
+          let lat = response.coord.lat;
+          let lon = response.coord.lon;
+          let icon = response.weather[0].icon;
+          $.ajax({
+            url: `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&appid=b45ecc5406887e49a29cd75e6b8dc427`,
+            method: "GET"
+          }).then(function(response) {
+              let uvi = response.current.uvi;
+              //change these variables into an object
+              displayCurrentWeather(temp, windspeed, humidity, uvi, cityname, icon);
+          });
+      });
+}
+
+function displayCurrentWeather(temp, windspeed, humidity, uvi, cityname, icon){
+let currentweatherData = `                    
+    <div>
+    <h3>${cityname} ${moment().format("D/M/YYYY")} <img id="iconimg" src="http://openweathermap.org/img/wn/${icon}@2x.png"></h3>
+    <p>Temprature: ${temp}</p>
+    <p>Humidity: ${humidity}</p>
+    <p>Wind Speed: ${windspeed}</p>
+    <p>UV Index: <span id="uvicolor">${uvi}</span></p>
+    </div>
+    `
+    $("#cardbox").append(currentweatherData);
+}
 function displayButtons(){
     for(let i = 0; i < 8; i++){
         let buttonhtml = $(`
